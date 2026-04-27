5c2ddd95438b218919af8e714f6d0e0e84d04ed2
rich62000-art <yjs1000@hanmail.net>
2026-04-27 19:04:12 +0900

    add privacy page

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
 
 import json
 import requests
